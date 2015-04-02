5c94ec607ea6766f4c1c93ef608cbaee531796fa
Allison Logan <agcandreva@yahoo.com>
2015-04-02 19:30:22 -0400

    adds footnotes to experience section

--- js/main.js
+++ js/main.js
@@ -1,42 +1,5 @@
 $(document).ready( function () {
 
-/*
-    var container = $( '#skill_tags' );
-
-    container.isotope({ filter: '.web' });
-    container.isotope({ filter: '.design' });
-
-/*
-    $('#web_btn').click( function() {
-        $('.design, .editing, .misc_skills, #design_btn, #editing_btn, #misc_btn').toggleClass('hide');
-    })//show/hide web skills
-
-    $('#design_btn').click( function() {
-        $('.web, .editing, .misc_skills, #web_btn, #editing_btn, #misc_btn').toggleClass('hide');
-    })//show/hide design skills
-
-    $('#editing_btn').click( function() {
-        $('.web, .design, .misc_skills, #design_btn, #web_btn, #misc_btn').toggleClass('hide');
-    })//show/hide editing skills
-
-    $('#misc_btn').click( function() {
-        $('.web, .design, .editing, #design_btn, #editing_btn, #web_btn').toggleClass('hide');
-    })//show/hide miscellaneous skills
-*/
-
-//Footnotes
-/*
-    var bigfoot = $.bigfoot(
-        {
-            activeOnUnhover: true,
-            deleteOnUnhover: true,
-            preventPageScroll: false,
-            hoverDelay: 250
-        }
-    );
-
-    $.bigfoot();*/
-
 //MixitUp Filtering
 
     $(function(){
@@ -54,12 +17,16 @@ $(document).ready( function () {
             var $anchor = $(this);
 
             $('html, body').stop().animate({
-                    scrollTop: $($anchor.attr('href')).offset().top
+                scrollTop: $($anchor.attr('href')).offset().top
             }, 1000);
 
             event.preventDefault();
         });
     });
 
+
+//Footnotes
+
+    $.bigfoot();
 
 });
