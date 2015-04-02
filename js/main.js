95ccd93f0c10e091a85794fc7e1dea2370f810f6
Allison Logan <agcandreva@yahoo.com>
2015-04-02 18:59:55 -0400

    adds filter plugin to skills tags

--- js/main.js
+++ js/main.js
@@ -1,5 +1,52 @@
 $(document).ready( function () {
 
+/*
+    var container = $( '#skill_tags' );
+
+    container.isotope({ filter: '.web' });
+    container.isotope({ filter: '.design' });
+
+/*
+    $('#web_btn').click( function() {
+        $('.design, .editing, .misc_skills, #design_btn, #editing_btn, #misc_btn').toggleClass('hide');
+    })//show/hide web skills
+
+    $('#design_btn').click( function() {
+        $('.web, .editing, .misc_skills, #web_btn, #editing_btn, #misc_btn').toggleClass('hide');
+    })//show/hide design skills
+
+    $('#editing_btn').click( function() {
+        $('.web, .design, .misc_skills, #design_btn, #web_btn, #misc_btn').toggleClass('hide');
+    })//show/hide editing skills
+
+    $('#misc_btn').click( function() {
+        $('.web, .design, .editing, #design_btn, #editing_btn, #web_btn').toggleClass('hide');
+    })//show/hide miscellaneous skills
+*/
+
+//Footnotes
+/*
+    var bigfoot = $.bigfoot(
+        {
+            activeOnUnhover: true,
+            deleteOnUnhover: true,
+            preventPageScroll: false,
+            hoverDelay: 250
+        }
+    );
+
+    $.bigfoot();*/
+
+//MixitUp Filtering
+
+    $(function(){
+    
+        // Instantiate MixItUp:
+    
+        $('#skills_tags').mixItUp();
+    
+    });
+
 //Slow scroll
 
     $(function() {
